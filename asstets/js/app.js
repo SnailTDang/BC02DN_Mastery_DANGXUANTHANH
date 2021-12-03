758af78464cd86b838a737a23cc43eb0764befc5
your name <xuanthanh.110699@gmail.com>
2021-12-04 01:32:49 +0700

    up

---
+++ asstets/js/app.js
@@ -0,0 +1,14 @@
+// $(document).ready(function(){
+//     $('.scroll-services').slick({
+//         // dots: true,
+//         arrows: true,
+//         speed: 800,
+//         autoplaySpeed: 5000,
+//         slidesToShow: 4,
+//         prevArrow: $('.pre'),
+//         nextArrow: $('.next'),
+//         infinite: true,
+//         autoplay: true,
+//         autoplaySpeed: 2000,
+//     });
+//   });
